13748dc40911924e99fab27e2b454544835a01c4
junhongkwon <60926717+junhongkwon@users.noreply.github.com>
2021-03-02 19:58:22 +0900

    Add files via upload

---
+++ UNIV2-1WEB/Main.php
@@ -0,0 +1,10 @@
+<?php
+     $name=$_REQUEST['name'];
+	 $email=$_REQUEST['email'];
+	 $opinion=$_REQUEST['opinion'];
+	 echo "NAME=$name<br>";
+	 echo "EMAIL=$email";
+	 echo "OPINION=$opinion";
+	 
+?>
+
